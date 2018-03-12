fe5c528d6b81a382d89714c5acb29c4be44c94f7
Saurabh <saurabhg@contractwrangler.com>
2018-03-12 19:19:32 +0530

    backend ready

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res){
+
+	var query = req.body.sql;
+
+	switch(query){
+		case "select * from table": 
+			res.json({
+				success: true,
+				verb: "GET table/_search",
+				queryDSL: { 
+					query: { 
+						match_all: {} 
+					} 
+				}
+			});
+			break;
+		default:
+			res.json({
+				success: false,
+				message: "Query cannot be converted."
+			});
+	}
+
+});
+
 module.exports = router;
